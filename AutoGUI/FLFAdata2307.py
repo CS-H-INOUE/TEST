72ccf8fb9632cad283e0dc20b7892062f9fce92a
CS-H-INOUE <112374412+CS-H-INOUE@users.noreply.github.com>
2023-07-20 09:11:51 +0900

    FLFACOPYOK,SendNG

--- AutoGUI/FLFAdata2307.py
+++ AutoGUI/FLFAdata2307.py
@@ -13,40 +13,42 @@ import openpyxl as px
 from openpyxl.utils import column_index_from_string
 import sys
 
-data = ['293212000', '293213000']
-
 # 一括削除関数
 def remove_glob(pathname, recursive=True):
     for p in glob.glob(pathname, recursive=recursive):
         if os.path.isfile(p):
             os.remove(p)
 
-def dldata():
-    # ここから目的に合わせて今回はfunc1-func4で動作させる。以下はfunc2の例
-    # 適宜データ用意し、同様に処理
+data = ['293212000', '293213000']
+
+# ダウンロードデータの処理関数
+def dldata(data):
+    # Chromeを起動しWebページにアクセス
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get('https://www.customs.go.jp/toukei/srch/index.htm?M=77&P=0')
+    driver.maximize_window()
+
+    # ダウンロードデータ処理
     for index, item in enumerate(data):
         print(index, item)
-        print("Call func1")
         if index == 0:
-            func1(item,data)
+            func1(item, data)
         elif index == 1:
-            func2(item,data)
+            func2(item, data)
 
     # DL OPEN
     pyautogui.sleep(5)
     pyautogui.click(x=100, y=1050)
+    driver.close()
 
-def output(data,i):
-    print("[debug: ]",data)
+# 出力処理
+def output(data, i):
+    print("[debug: ]", data)
 
-    # 
-    # 
-    # Output -> CSV
-    # 
-    # 
+    # CSVをEXCELに変換
     time.sleep(5)
     pyautogui.click(x=40, y=420)
-    for i in range(1):
+    for _ in range(1):
         pyautogui.press('tab')
     time.sleep(1)
     pyautogui.press('enter')
@@ -55,162 +57,68 @@ def output(data,i):
 
     if i == 1:
         pyautogui.keyDown('shift')
-        for i in range(2):
+        for _ in range(2):
             pyautogui.press('tab')
         pyautogui.keyUp('shift')
     pyautogui.press('enter')
     print("-------------------再検索-------------------")
 
 # func1: 特定の国コードを指定して検索し、CSVをダウンロードする
-def func1(n,data):
-
-    # 
-    # 
-    # SearchPage(https://www.customs.go.jp/toukei/srch/index.htm?M=77&P=...)
-    # 
-    # 
-
+def func1(n, data):
+    # 検索条件入力
     pyautogui.sleep(6)
-    # pyautogui.click(x=40, y=375)
-    pyautogui.sleep(1)
-    # pyautogui.press('tab')
     pyautogui.press('right')
-    print("輸入")
-
-    for i in range(2):
+    for _ in range(2):
         pyautogui.press('tab')
     time.sleep(1)
     pyautogui.press('space')
     time.sleep(1)
-    for i in range(5):
+    for _ in range(5):
         pyautogui.press('down')
     time.sleep(1)
     pyautogui.press('enter')
-    print("get year")
-
-    for i in range(3):
+    for _ in range(3):
         pyautogui.press('tab')
     pyautogui.press('space')
     pyautogui.press('down')
     pyautogui.press('enter')
     pyautogui.press('tab')
     time.sleep(1)
-    # 105:China
     pyautogui.typewrite('105')
-    # time.sleep(1)
-    for i in range(3):
+    for _ in range(3):
         pyautogui.press('tab')
-    print("get country")
-
     pyautogui.press('space')
     pyautogui.press('down')
     pyautogui.press('enter')
     pyautogui.press('tab')
     pyautogui.typewrite(n)
-    # time.sleep(1)
     pyautogui.press('enter')
-
     print("TestFinished")
-    i=1
-    output(data,i)
+    output(data, 1)
 
-def func2(n,data):
+# func2: 品目コードを指定して検索し、CSVをダウンロードする
+def func2(n, data):
     time.sleep(4)
     print("Select code...........")
-    for i in range(10):
+    for _ in range(10):
         pyautogui.press('tab')
-    # time.sleep(1)
-    for i in range(12):
+    for _ in range(12):
         pyautogui.press('backspace')
-    # time.sleep(1)
     pyautogui.typewrite(n)
     pyautogui.press('enter')
-
     print("goto OutputPage")
-    i=2
-    output(data,i)
-
-# func3: 国コードと品目コードを指定して検索し、CSVをダウンロードする
-def func3(country_num, n):
-    # TAB+ALT
-    pyautogui.sleep(5)
-    pyautogui.keyDown('alt')
-    pyautogui.press('tab')
-    pyautogui.keyUp('alt')
-
-    pyautogui.sleep(1)
-    pyautogui.click(x=40, y=375)
-
-    # Number INPUT
-    pyautogui.sleep(2)
-    for i in range(6):
-        pyautogui.press('tab')
-    pyautogui.typewrite(country_num)
-
-    for i in range(4):
-        pyautogui.press('tab')
-
-    # Type Keyboard
-    pyautogui.typewrite(n)
-
-    # Search
-    pyautogui.press('enter')
-
-    # CSV DL
-    pyautogui.sleep(3)
-    pyautogui.click(x=70, y=460)
-
-# func4: 品目コードを指定して検索し、CSVをダウンロードする
-def func4(n):
-    # TAB+ALT
-    pyautogui.sleep(5)
-    pyautogui.keyDown('alt')
-    pyautogui.press('tab')
-    pyautogui.keyUp('alt')
-
-    pyautogui.sleep(1)
-    pyautogui.click(x=40, y=375)
-
-    # Number INPUT
-    # Select All Country
-    pyautogui.sleep(2)
-    for i in range(5):
-        pyautogui.press('tab')
-    pyautogui.press('up')
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-
-    # Type Keyboard
-    pyautogui.press('delete')
-    pyautogui.typewrite(n)
-
-    # Search
-    pyautogui.press('enter')
-
-    # CSV DL
-    pyautogui.sleep(3)
-    pyautogui.click(x=70, y=460)
-
-# ダウンロード関数
-def exebrowser():
-    # run chrome
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-
-    driver.get('https://www.customs.go.jp/toukei/srch/index.htm?M=77&P=0')
-    driver.maximize_window()
-
-    # dlデータ関数へ
-    dldata()
-    driver.close()
+    output(data, 2)
 
 try:
     # ホームディレクトリまでのパスを取得
     p_path = os.path.expanduser("~")
-    remove_glob(p_path + 'Downloads/*.zip')
+    remove_glob(p_path + '/Downloads/*.zip')
+    print("Removing DownloadFolder.......")
+
     x, y = pyautogui.position()
 
     # exebrowser関数の実行
-    exebrowser()
+    dldata(data)
 
     # ダウンロードファイルを一時フォルダに解凍し、リネームする
     today = datetime.date.today()  # 今日の日付を取得
@@ -219,11 +127,11 @@ try:
     D = str(today.day)
 
     # Path形式でDownloads以下のzipファイルを l_1にlistで格納
-    p = Path(p_path + "Downloads/")
+    p = Path(p_path, "Downloads/")
     l_1 = list(p.glob('*.zip'))
     re_l = ['fl.csv', 'fa.csv']
 
-    Temp_P = Path(p_path + "/DESKTOP/temp_python")
+    Temp_P = Path(p_path, "DESKTOP/temp_python")
 
     # listの要素数から、zipfile.ZipFileリストをmyzipに格納
     for i in range(len(l_1)):
@@ -236,7 +144,7 @@ try:
     i = 0
 
     # 格納先フォルダの作成
-    NEW_P = Path(p_path + "/DESKTOP/temp_python/RENAME(PY_AUTO)/")
+    NEW_P = Path(p_path, "DESKTOP/temp_python/RENAME(PY_AUTO)/")
     if not os.path.exists(NEW_P):
         os.mkdir(NEW_P)
     else:
@@ -260,27 +168,35 @@ try:
         print("newFileName:", newFileName)
         print("newFilePath:", newFilePath)
 
+        print("[debug]",files)
+
         os.rename(oldFilePath, newFilePath)
         i += 1
 
     # CSVをEXCELに変換し、データをコピー＆ペーストする
-    with open(p_path + 'Desktop/temp_python/RENAME(PY_AUTO)/.csv', newline='') as tempfile:
-        data_list_fl = list(csv.reader(tempfile, delimiter=','))
+    for csv_file in re_l:
+        csv_path = os.path.join(p_path, f'Desktop/temp_python/RENAME(PY_AUTO)/{csv_file}')
+        print("[debug]",csv_path)
+        csv_file_path = os.path.join(p_path, 'Desktop/temp_python/RENAME(PY_AUTO)', csv_file)
+        with open(csv_file_path, 'r', encoding='shift-jis', newline='') as tempfile:
+            data_list_fl = list(csv.reader(tempfile, delimiter=','))
 
-    wb = px.Workbook()
-    ws = wb.active
-    ws.append([])  # A1, B1 空行
+        wb = px.Workbook()
+        ws = wb.active
+        ws.append([])  # A1, B1 空行
 
-    # データの格納
-    for record in data_list_fl:
-        ws.append(record)
+        # データの格納
+        for record in data_list_fl:
+            ws.append(record)
 
-    # xlsx形式で保存
-    wb.save(p_path + 'Desktop/temp_python/RENAME(PY_AUTO)/temp.xlsx')
+        # xlsx形式で保存
+        xlsx_file = os.path.splitext(csv_file)[0] + '.xlsx'
+        xlsx_path = os.path.join(p_path, f'Desktop/temp_python/RENAME(PY_AUTO)/{xlsx_file}')
+        wb.save(xlsx_path)
 
     # データを別のブックにコピー＆ペースト
-    temp = p_path + 'Desktop/temp_python/RENAME(PY_AUTO)/temp.xlsx'
-    output = p_path + 'Desktop/test.xlsx'
+    temp = os.path.join(p_path, 'Desktop/temp_python/RENAME(PY_AUTO)/temp.xlsx')
+    output = os.path.join(p_path, 'Desktop/test.xlsx')
 
     wb_temp = px.load_workbook(filename=temp)
     ws_temp = wb_temp.active
